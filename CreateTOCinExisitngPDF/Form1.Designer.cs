13f7de8cae4a118eb23665f7ea6451d5eae4b820
Asad Ali <asad.ali@aspose.com>
2020-05-09 01:30:08 +0500

    Add files via upload

---
+++ CreateTOCinExisitngPDF/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CreateTOCinExisitngPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPDF = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnExtractTitles = new System.Windows.Forms.Button();
+            this.btnCreateTOC = new System.Windows.Forms.Button();
+            this.btnCreatePDF = new System.Windows.Forms.Button();
+            this.lstTitles = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Location = new System.Drawing.Point(32, 36);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.ReadOnly = true;
+            this.txtLicense.Size = new System.Drawing.Size(269, 20);
+            this.txtLicense.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLicense);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 87);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select License File";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPDF);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 87);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select PDF File";
+            // 
+            // txtPDF
+            // 
+            this.txtPDF.Location = new System.Drawing.Point(32, 36);
+            this.txtPDF.Name = "txtPDF";
+            this.txtPDF.ReadOnly = true;
+            this.txtPDF.Size = new System.Drawing.Size(269, 20);
+            this.txtPDF.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExtractTitles
+            // 
+            this.btnExtractTitles.Location = new System.Drawing.Point(12, 198);
+            this.btnExtractTitles.Name = "btnExtractTitles";
+            this.btnExtractTitles.Size = new System.Drawing.Size(414, 23);
+            this.btnExtractTitles.TabIndex = 5;
+            this.btnExtractTitles.Text = "Extract First Phrase/Word from Each Page";
+            this.btnExtractTitles.UseVisualStyleBackColor = true;
+            this.btnExtractTitles.Click += new System.EventHandler(this.btnExtractTitles_Click);
+            // 
+            // btnCreateTOC
+            // 
+            this.btnCreateTOC.Location = new System.Drawing.Point(12, 390);
+            this.btnCreateTOC.Name = "btnCreateTOC";
+            this.btnCreateTOC.Size = new System.Drawing.Size(414, 23);
+            this.btnCreateTOC.TabIndex = 7;
+            this.btnCreateTOC.Text = "Create TOC in Selected PDF with Unique Titles";
+            this.btnCreateTOC.UseVisualStyleBackColor = true;
+            this.btnCreateTOC.Click += new System.EventHandler(this.btnCreateTOC_Click);
+            // 
+            // btnCreatePDF
+            // 
+            this.btnCreatePDF.Location = new System.Drawing.Point(12, 419);
+            this.btnCreatePDF.Name = "btnCreatePDF";
+            this.btnCreatePDF.Size = new System.Drawing.Size(414, 23);
+            this.btnCreatePDF.TabIndex = 8;
+            this.btnCreatePDF.Text = "Create Separate PDF file for lenghty Chapters and Create TOC for rest";
+            this.btnCreatePDF.UseVisualStyleBackColor = true;
+            this.btnCreatePDF.Click += new System.EventHandler(this.btnCreatePDF_Click);
+            // 
+            // lstTitles
+            // 
+            this.lstTitles.FormattingEnabled = true;
+            this.lstTitles.Location = new System.Drawing.Point(12, 227);
+            this.lstTitles.Name = "lstTitles";
+            this.lstTitles.Size = new System.Drawing.Size(414, 160);
+            this.lstTitles.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 454);
+            this.Controls.Add(this.lstTitles);
+            this.Controls.Add(this.btnCreatePDF);
+            this.Controls.Add(this.btnCreateTOC);
+            this.Controls.Add(this.btnExtractTitles);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create TOC in Existing PDF using Aspose.PDF for .NET";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPDF;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExtractTitles;
+        private System.Windows.Forms.Button btnCreateTOC;
+        private System.Windows.Forms.Button btnCreatePDF;
+        private System.Windows.Forms.ListBox lstTitles;
+    }
+}
+
---
+++ CreateTOCinExisitngPDF/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+
+namespace CreateTOCinExisitngPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "License Files (*.lic)|*.lic|All files (*.*)|*.*";
+            fd.InitialDirectory = @"C:\";
+            fd.Title = "Please select a License file to Use.";
+            fd.Multiselect = false;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txtLicense.Text = fd.FileName;
+                License lic = new License();
+                lic.SetLicense(txtLicense.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "PDF Files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            fd.InitialDirectory = @"C:\";
+            fd.Title = "Please select a PDF file to upload.";
+            fd.Multiselect = false;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txtPDF.Text = fd.FileName;
+                lstTitles.Items.Clear();
+            }
+        }
+
+        private void btnExtractTitles_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(txtLicense.Text) && !String.IsNullOrEmpty(txtPDF.Text))
+                {
+                    Document doc = new Document(txtPDF.Text);
+                    foreach (Page page in doc.Pages)
+                    {
+                        TextFragmentAbsorber absorber = new TextFragmentAbsorber();
+                        page.Accept(absorber);
+                        if (lstTitles.Items.Contains(absorber.TextFragments[1].Text))
+                        { continue; }
+                        lstTitles.Items.Add(absorber.TextFragments[1].Text);
+                        page.Dispose();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select License and PDF file first.", "No File Selected");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void btnCreateTOC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstTitles.Items.Count > 0)
+                {
+                    Document doc = new Document(txtPDF.Text);
+                    Page tocPage = doc.Pages.Insert(1);
+
+                    // Create object to represent TOC information
+                    TocInfo tocInfo = new TocInfo();
+                    TextFragment title = new TextFragment("Table Of Contents");
+                    title.TextState.FontSize = 20;
+                    title.TextState.FontStyle = FontStyles.Bold;
+
+                    // Set the title for TOC
+                    tocInfo.Title = title;
+                    tocPage.TocInfo = tocInfo;
+                    foreach (var item in lstTitles.Items)
+                    {
+                        TextFragmentAbsorber absorber = new TextFragmentAbsorber(item.ToString());
+                        doc.Pages.Accept(absorber);
+                        Page targetPage = doc.Pages[absorber.TextFragments[1].Page.Number];
+                        // Create Heading object
+                        Aspose.Pdf.Heading heading2 = new Aspose.Pdf.Heading(1);
+                        TextSegment segment2 = new TextSegment();
+                        heading2.TocPage = tocPage;
+                        heading2.Segments.Add(segment2);
+                        // Specify the destination page for heading object
+                        heading2.DestinationPage = targetPage;
+                        // Destination page
+                        heading2.Top = targetPage.Rect.Height;
+                        // Destination coordinate
+                        segment2.Text = item.ToString();
+                        // Add heading to page containing TOC
+                        tocPage.Paragraphs.Add(heading2);
+                    }
+                    doc.Save("Toc_out.pdf");
+                    MessageBox.Show("PDF with TOC has been created.", "Success");
+                }
+                else
+                {
+                    MessageBox.Show("Please extract titles from PDF document first.", "Titles are not extracted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void btnCreatePDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstTitles.Items.Count > 0)
+                {
+                    Dictionary<string, List<int>> titles = new Dictionary<string, List<int>>();
+                    List<string> ts = new List<string>();
+                    Document doc = new Document(txtPDF.Text);
+                    foreach (Page page in doc.Pages)
+                    {
+                        TextFragmentAbsorber absorber = new TextFragmentAbsorber();
+                        page.Accept(absorber);
+                        if (titles.Where(x => x.Key == absorber.TextFragments[1].Text).ToList().Count > 0)
+                        {
+                            titles[absorber.TextFragments[1].Text].Add(absorber.TextFragments[1].Page.Number);
+                            ts.Remove(absorber.TextFragments[1].Text);
+                        }
+                        else
+                        {
+                            titles.Add(absorber.TextFragments[1].Text, new List<int>() { absorber.TextFragments[1].Page.Number });
+                            ts.Add(absorber.TextFragments[1].Text);
+                        }
+                    }
+                    // Get chapters which are more than 1 page
+                    var pages = titles.Where(x => x.Value.Count > 1).Select(x => x.Value).ToList();
+                    List<int> ps = new List<int>();
+                    if (pages.Count > 0)
+                    {
+                        foreach (var page in pages)
+                        {
+                            Document newDoc = new Document();
+                            foreach (var p in page)
+                            {
+                                newDoc.Pages.Add(doc.Pages[p]);
+                                ps.Add(p);
+                            }
+                            newDoc.Save("newPDF_" + DateTime.Now.Millisecond + ".pdf");
+                        }
+                        // Delete extracted pages from existing PDF
+                        doc.Pages.Delete(ps.ToArray());
+                        doc.Save("tmp.pdf");
+                    }
+                    // Create TOC for remaining pages
+                    doc = new Document("tmp.pdf");
+                    Page tocPage = doc.Pages.Insert(1);
+
+                    // Create object to represent TOC information
+                    TocInfo tocInfo = new TocInfo();
+                    TextFragment title = new TextFragment("Table Of Contents");
+                    title.TextState.FontSize = 20;
+                    title.TextState.FontStyle = FontStyles.Bold;
+
+                    // Set the title for TOC
+                    tocInfo.Title = title;
+                    tocPage.TocInfo = tocInfo;
+                    foreach (var s in ts)
+                    {
+                        TextFragmentAbsorber absorber = new TextFragmentAbsorber(s);
+                        doc.Pages.Accept(absorber);
+                        Page targetPage = doc.Pages[absorber.TextFragments[1].Page.Number];
+                        // Create Heading object
+                        Aspose.Pdf.Heading heading2 = new Aspose.Pdf.Heading(1);
+                        TextSegment segment2 = new TextSegment();
+                        heading2.TocPage = tocPage;
+                        heading2.Segments.Add(segment2);
+                        // Specify the destination page for heading object
+                        heading2.DestinationPage = targetPage;
+                        // Destination page
+                        heading2.Top = targetPage.Rect.Height;
+                        // Destination coordinate
+                        segment2.Text = s;
+                        // Add heading to page containing TOC
+                        tocPage.Paragraphs.Add(heading2);
+                    }
+                    doc.Save("Toc_" + DateTime.Now.Millisecond + ".pdf");
+                    File.Delete("tmp.pdf");
+                    MessageBox.Show("PDF with TOC has been created.", "Success");
+                }
+                else
+                {
+                    MessageBox.Show("Please extract titles from PDF document first.", "Titles are not extracted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
